4f9184a4f7f84f25c7f8eeb70d5d7cf0ae5cca89
abtiwari797 <abtiwari797@gmail..com>
2025-11-11 19:04:55 +0530

    feat: udpate to firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,7 +2,10 @@ import admin from "firebase-admin";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const serviceAccount = require("../../firebaseServiceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
+
+// Replace literal \n with actual newlines in private_key
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
