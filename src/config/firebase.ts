0db55afbba4564ceb0e95c5f8b36c83cda519c6c
abtiwari797 <abtiwari797@gmail..com>
2025-11-11 19:34:47 +0530

    feat: udpate to firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,10 +2,19 @@ import admin from "firebase-admin";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
-
-// Replace literal \n with actual newlines in private_key
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+const serviceAccount = {
+  type: "service_account",
+  projectId: "petsnapchat-188ad",           // camelCase
+  privateKeyId: "a39ab3e56db9bfe213bba038381ebf1d0ee3c27d",
+  privateKey: `-----BEGIN PRIVATE KEY-----\nMIIEv...snip...\n-----END PRIVATE KEY-----\n`, // literal newlines
+  clientEmail: "firebase-adminsdk-fbsvc@petsnapchat-188ad.iam.gserviceaccount.com",
+  clientId: "113579521094223818716",
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+  tokenUri: "https://oauth2.googleapis.com/token",
+  authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
+  clientX509CertUrl: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40petsnapchat-188ad.iam.gserviceaccount.com",
+  universeDomain: "googleapis.com"
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
